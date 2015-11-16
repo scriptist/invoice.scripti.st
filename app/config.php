083d16a9126a8d105dc2eb317b783f314170bc4f
Michael Berman <me@scripti.st>
2015-11-16 20:55:54 +0100

    Built unstyled invoice page

--- app/config.php
+++ app/config.php
@@ -4,10 +4,18 @@ global $dbCredentials;
 $dbCredentials = [
     'database_type' => 'mysql',
     'database_name' => 'invoice',
-    'server' => 'localhost',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8'
+    'server'        => 'localhost',
+    'username'      => 'root',
+    'password'      => '',
+    'charset'       => 'utf8'
+];
+
+global $billingInfo;
+$billingInfo = [
+	'name'    => 'Joe Schmoe',
+	'address' => "123 Fake Street\nSneezeborough\nRabbitton",
+	'payment' => "Bank: Sneezeborough Bank\nAccount name: Joe Schmoe\nAccount Number: 1234 5678",
+	'email'   => 'joeschmoe@example.com'
 ];
 
 ?>
---
+++ app/routes/invoice.php
@@ -0,0 +1,27 @@
+<?php
+
+$app->get('/invoice/:id', function ($id) use ($app) {
+	global $billingInfo;
+	global $dbCredentials;
+	$database = new medoo($dbCredentials);
+	$invoice = $database->get("invoice",
+		["id", "modified", "company", "name", "address", "type"],
+		["id" => $id]
+	);
+
+	if ($invoice) {
+		$invoice['lines'] = $database->select("line",
+			["id", "description", "charge", "quantity", "is_hours"],
+			[
+				"invoice_id" => $invoice['id'],
+				"ORDER" => "id ASC"
+			]
+		);
+	} else {
+		$app->response->setStatus(404);
+	}
+
+	$app->render('invoice.html.twig', ['invoice'=>$invoice, 'billing'=>$billingInfo]);
+})->setName('invoice');
+
+?>
