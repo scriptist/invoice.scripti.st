6bf603315855d88b8c10a233c4c22ecce7f6ed63
Michael Berman <me@scripti.st>
2015-12-02 10:50:12 +0100

    Added ABN

--- config.php
+++ config.php
@@ -13,6 +13,7 @@ $dbCredentials = [
 global $billingInfo;
 $billingInfo = [
 	'name'    => 'Joe Schmoe',
+	'abn'     => '00 000 000 000',
 	'address' => "123 Fake Street\nSneezeborough\nRabbitton",
 	'payment' => "Bank: Sneezeborough Bank\nAccount name: Joe Schmoe\nAccount Number: 1234 5678",
 	'email'   => 'joeschmoe@example.com'
